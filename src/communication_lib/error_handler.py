f7fa57e7129b01edc08a68cc57e8d0c2d4bc63e5
olili27 <timmolili27@gmail.com>
2024-07-05 08:33:05 +0300

    commit any previously missed changes

--- src/communication_lib/error_handler.py
+++
@@ -1,6 +0,0 @@
-class ErrorHandler:
-    def __init__(self, critical_error_callback):
-        self.critical_error_callback = critical_error_callback
-
-    def handle_error(self, error_message):
-        self.critical_error_callback(error_message)
--- src/communication_lib/sequence_manager.py
+++
@@ -1,7 +0,0 @@
-class SequenceManager:
-    def __init__(self):
-        self.current_sequence = 0
-
-    def get_next_sequence(self):
-        self.current_sequence += 1
-        return self.current_sequence
