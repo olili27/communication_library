f14a4ecc22750df4caf64580b5321f34549f18ae
olili27 <timmolili27@gmail.com>
2024-07-08 10:14:57 +0300

    add attributes and methods to the httpHandler

---
+++ src/communication_lib/httpHandler.mjs
@@ -0,0 +1,36 @@
+class HttpHandler {
+    #xhr = new XMLHttpRequest();
+
+    #isStatusBetween200InclusiveAnd300(status) {
+        return status >= 200 && status < 300;
+    }
+    
+    sendRequest(method, fullEndpoint, payload, onSuccessCallback, onErrorCallback) {
+        if(!method) {
+            throw "No method given"
+        }
+
+        if(!fullEndpoint) {
+            throw "No full endpoint given"
+        }
+
+        this.#xhr.open(method.toUpperCase(), fullEndpoint, true)
+
+        this.#xhr.setRequestHeader("Content-Type", "application/json");
+
+        this.#xhr.onerror = function() {
+            console.log("there was an error!");
+            return
+        }
+        
+        const self = this;
+        this.#xhr.onload = function() {
+            if(self.#isStatusBetween200InclusiveAnd300(self.#xhr.status) && self.#xhr.readyState == 4) {
+                onSuccessCallback(this.responseText)
+                return
+            }
+        }
+
+        Object.keys(payload).length ? this.#xhr.send(JSON.stringify(payload)) : this.#xhr.send();
+    }
+}
