1e80234d0dca998e85dad2de915388fba17d2269
olili27 <timmolili27@gmail.com>
2024-07-25 19:26:06 +0300

    test the new changes in destinationQueue.mjs

--- src/tests/t_queueManager.mjs
+++ src/tests/t_queueManager.mjs
@@ -1,21 +1,34 @@
 import { QueueManger } from "../communication_lib/destinationQueueManager.mjs";
+import { PayloadWrapper } from "../communication_lib/wrapper.mjs";
+import { SequenceManager } from "../communication_lib/sequenceManager.mjs";
 
-let queueManager = new QueueManger()
-console.log(queueManager.getQueue("some"))
+
+let wrappedMessage = new PayloadWrapper(new SequenceManager()).wrapPayload("some randomId", "http://localhost:5500", "testing")
+let queueManager = new QueueManger();
+
+console.log(queueManager.getQueue(wrappedMessage.destination))
 queueManager.addToDestinationQueue(
-  { branchId: "some randomId", date: "2024-03-23", sequenceNumber:2 },
+  wrappedMessage,
   "getCashBook"
 );
-queueManager.addToDestinationQueue(
-  { branchId: "some randomId", date: "2024-03-23", sequenceNumber: 4 },
-  "getCashBook"
-);
-console.log(queueManager);
 
-console.log(queueManager.isEmpty(queueManager.getQueue("getCashBook")));
-console.log(queueManager.removeFromDestinationQueue("getCashBook"));
-console.log(queueManager.isEmpty(queueManager.getQueue("getCashBook")));
-console.log(queueManager.removeFromDestinationQueue("getCashBook"));
-console.log(queueManager.isEmpty(queueManager.getQueue("getCashBook")));
-console.log(queueManager.removeFromDestinationQueue("getCashBook3"));
+// queueManager.addToDestinationQueue(
+//   { branchId: "some randomId", date: "2024-03-23", sequenceNumber: 4 },
+//   "getCashBook"
+// );
 console.log(queueManager);
+console.log(queueManager.queues);
+
+console.log(
+  queueManager.isEmpty(queueManager.getQueue(wrappedMessage.destination))
+);
+
+console.log(queueManager.popFromQueue("http"));
+
+console.log(
+  queueManager.isEmpty(queueManager.getQueue(wrappedMessage.destination))
+);
+// console.log(queueManager.removeFromDestinationQueue("getCashBook"));
+// console.log(queueManager.isEmpty(queueManager.getQueue("getCashBook")));
+// console.log(queueManager.removeFromDestinationQueue("getCashBook3"));
+// console.log(queueManager);
