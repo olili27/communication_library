07eeb07a58bf7c8bdc81c0df784e7d9d0fab879a
olili27 <timmolili27@gmail.com>
2024-07-27 20:47:13 +0300

    integrate and create common functionality with all other classes. It is the entry point to setting up the commLibrary i.e sending and receiving messages to the desired endpoints

--- src/communication_lib/communicationLib.mjs
+++ src/communication_lib/communicationLib.mjs
@@ -1,21 +1,61 @@
 import { HttpHandler } from "./httpHandler.mjs"
 import { WebsocketHandler } from "./websocketHandler.mjs"
-import { ErrorHandler } from "./errorHandler.mjs"
 import { PayloadWrapper } from "./wrapper.mjs"
 import { QueueManger } from "./destinationQueueManager.mjs"
+import { SequenceManager } from "./sequenceManager.mjs"
 
 
-class CommunicationLib {
-    #httpHandler = "";
-    isOpen = true;
+export class CommunicationLib {
+  isInitialized = false;
 
-    constructor(errorCallback) {
-        this.errorCallback = errorCallback;
-        this.sequenceManager = new QueueManger();
-        this.payloadWrapper = new PayloadWrapper(this.sequenceManager);
-        this.websocketHandler = new WebsocketHandler();
-        this.httpHandler = new HttpHandler();
-    }
+  constructor(errorCallback) {
+    this.errorCallback = errorCallback;
+    this.queueManager = new QueueManger();
+    this.sequenceManager = new SequenceManager();
+    this.payloadWrapper = new PayloadWrapper(this.sequenceManager);
+    this.websocketHandler;
+    this.httpHandler = new HttpHandler();
+    this.startCommunicationModule()
+  }
 
+  sendMessage(message, url, tag) {
+    let wrappedMessage = this.payloadWrapper.wrapPayload(message, url, tag);
+    this.queueManager.addToDestinationQueue(wrappedMessage, this.errorCallback);
+  }
 
+  startCommunicationModule() {
+    let self = this;
+    this.isInitialized = true;
+    setInterval(function() {
+      self.processHttpQueue();
+      self.processWssQueue()
+    }, 1000)
+  }
+
+  async processHttpQueue() {
+      let wrappedMessage = this.queueManager.popFromQueue("http");
+
+      if(wrappedMessage) {
+         this.httpHandler.sendRequest(
+           "post",
+           wrappedMessage.destination,
+           wrappedMessage,
+           this.successCallBack,
+           this.errorCallback
+         );
+      }
+  }
+
+  successCallBack(data) {
+    console.log("http response: ",data);
+  }
+
+  async processWssQueue() {
+      let wrappedMessage = this.queueManager.popFromQueue("ws");
+
+      if(wrappedMessage) {
+        this.websocketHandler = new WebsocketHandler(wrappedMessage.destination)
+        this.websocketHandler.sendMessage(wrappedMessage);
+      }
+  }
 }
